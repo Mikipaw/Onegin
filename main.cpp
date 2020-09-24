a39fec52ce087c497bf7550d6c6b17741ec4c0cb
Mikipaw <71548305+Mikipaw@users.noreply.github.com>
2020-09-24 23:30:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
  *  \author    Michael Pavlov
  *  \author    Mikhail Pavlov
  *  \version   C++20
- *  \date      22.09.2020
+ *  \date      09.2020
  *  \pre       First initialize the system.
  *  \bug       No
  *  \warning
@@ -18,6 +18,7 @@
 #include "Tests.h"
 
 #define END_OF_FILE "~"
+#define END_OF_FILE_SYM '~'
 
 /*!
 *  \function void Print(FILE* input_file, FILE* output_file)
@@ -89,7 +90,7 @@ void Filling_The_Array(char** text, char*** pointers, int number_of_lines){}
 int print_n_count_number_of_lines(const char* text, FILE* result_file_name){
     int result = 0;
     int c = 0, i = 0;
-    while(c != '~'){
+    while(c != END_OF_FILE_SYM){
         c = (char)text[i];
         if((char)c == '\0'){
             result++;
@@ -105,21 +106,24 @@ int print_n_count_number_of_lines(const char* text, FILE* result_file_name){
 char* text_from_file(FILE* file_name){
     //ftell
     fseek(file_name, 0, SEEK_SET);
-    char* result_string = (char*)malloc(4096);
+    fseek(file_name, 0, SEEK_END); // seek to end of file
+    size_t size_of_file = ftell(file_name); // get current file pointer
+    fseek(file_name, 0, SEEK_SET); // seek back to beginning of file
+    char* result_string = (char*)malloc(size_of_file);
     char c = 0;
     int i = 0;
 
     while(!feof(file_name)){
         c = (char)fgetc(file_name);
-        if (c != '\n')
+        if (c != '\n'){
             result_string[i++] = c;
-        else
-            result_string[i++] = '\0';
+        }
+        else if(result_string[i - 1] != '\0') result_string[i++] = '\0';
     }
     result_string[i++] = '\n';
     result_string[i++] = '\0';
-    result_string[i++] = '~';
-    result_string[i] = '\0';
+    result_string[i++] =  '~';
+    result_string[i]   = '\0';
 
     return result_string;
 }
@@ -127,8 +131,8 @@ char* text_from_file(FILE* file_name){
 void Interface(){
 
     //Program is opening the file "Romeo and Juliet"
-    FILE* original_text = fopen("C://Programming/projects/Onegin/simpleTest.txt", "rb");
-    FILE* result_file = fopen("C://Programming/projects/Onegin/Result.txt", "wb");
+    FILE* original_text = fopen("C://Programming/projects/Onegin/Test.txt", "rb");
+    FILE* result_file   = fopen("C://Programming/projects/Onegin/Result.txt", "wb");
 
     if(original_text == nullptr || result_file == nullptr){
         fprintf(stderr, "Error opening file!\n");
@@ -136,59 +140,62 @@ void Interface(){
     }
 
     char* text = text_from_file(original_text);
-    fclose(original_text);
-    //printf("%s", text);
 
     //Program prints original text on the screen
     int number_of_lines = print_n_count_number_of_lines(text, result_file);
-    //int number_of_lines = 7022;
-    printf("%d\n", number_of_lines);
-    //char* text;
-    //text = text_from_file(original_text);
+    printf("Number of lines in input text - %d\n", number_of_lines);
+
     char** pointers = (char**) calloc(number_of_lines + 1, sizeof(char*));
-    //Filling_The_Array(&text, &pointers, number_of_lines);
-    //pointers = (char**) calloc(number_of_lines, sizeof(char));
-    int j = 0;
-    pointers[0] = (char*) calloc(50, sizeof(char));
+
+    pointers[0] = (char*) calloc(60, sizeof(char));
     pointers[0] = text;
 
+    int pos = 0;
     for(int i = 1; i < number_of_lines; ++i){
-        pointers[i] = (char*) calloc(50, sizeof(char));
-        while(text[j] != '\0') ++j;
-        while(text[j] == '\0') ++j;
-        pointers[i] = &text[j++];
+        pointers[i] = (char*) calloc(60, sizeof(char));
+        while(text[pos] != '\0') ++pos;
+        while(text[pos] == '\0') ++pos;
+        pointers[i] = &text[pos++];
     }
-    int a[150] = {};
     _flushall();
-    printf("Complete!\n");
-    printf("Complete!\n");
-    for(int i = 0; i < number_of_lines; ++i) printf("%s\n", pointers[i]);
+
+    //Program prints original text in the file
+    fprintf(result_file,"Original text:\n");
+    for(int i = 0; i < number_of_lines; ++i) fprintf(result_file, "%s\n", pointers[i]);
 
     quick_sort_strings(pointers, 0, number_of_lines - 1);
     //qsort(pointers, number_of_lines, sizeof(char*),
           //(int(*)(const void*, const void*))str_cmp_4_sort);
-    for(int i = 0; i < number_of_lines; ++i) printf("%s\n", pointers[i]);
 
-    //for(int i = 0; i < number_of_lines; ++i) printf("%s\n", &pointers[i]);
-
-    //Program prints sorted text on the screen
-    //PrintSorted     (&text);
-
-    //Program prints reversed text on the screen
-    //PrintReversed   (&text);
-
-    //Program shows the original text on the screen again
-    //Print           (&text);
-    /*for(int i = 0; i < number_of_lines; ++i){
-        free(pointers[i]);
+    //Program prints sorted text in the file
+    fprintf(result_file, "\nSorted from begin:\n");
+    for(int i = 0; i < number_of_lines; ++i) {
+        if(str_cmp(pointers[i], "\n") == FALSE)
+            fprintf(result_file, "%s\n", pointers[i]);
     }
-     */
 
-    free(text);
+    //Program prints sorted from end text in the file
+    fprintf(result_file, "\nSorted from end:\n");
+    quick_sort_strings(pointers, 0, number_of_lines - 1, "FROM END");
+    for(int i = 0; i < number_of_lines; ++i){
+        if(str_cmp(pointers[i], "\0") == 0)
+            fprintf(result_file, "%s\n", pointers[i]);
+    }
+
+    //Program prints original text again
+    fprintf(result_file, "\nFinal text:\n");
+    fseek(original_text, 0, SEEK_SET);
+    char c = 0;
+    while(!feof(original_text)) {
+        c = (char) fgetc(original_text);
+        fputc(c, result_file);
+    }
+
+    //Closing files and free the memory
+    fclose(original_text);
+    fclose(result_file);
     for(int o = 0; o < number_of_lines; ++o) {
         free(pointers[o]);
     }
     free(pointers);
 }
-
-
