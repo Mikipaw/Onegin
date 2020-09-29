50dd4828f39843b36de759ba552d4169064e7e61
Mikipaw <71548305+Mikipaw@users.noreply.github.com>
2020-09-29 21:31:44 +0300

    Update Tests.h

--- Tests.h
+++ Tests.h
@@ -1,7 +1,8 @@
 //
-// Created by pawlo on 20.09.2020.
+// Created by mikipaw on 20.09.2020.
 //
 #pragma once
+
 #ifndef ONEGIN_TESTS_H
 #define ONEGIN_TESTS_H
 
@@ -17,27 +18,54 @@ void AllTests(){
     fprintf(stderr, "All tests are OK!\n");
 }
 
-void strcmp_Test(){
-    const char* uno = "Milk";
-    const char* dos = "Milk";
-    const char* trs = "NotMilk";
+void strcmp_Test() {
+    simple_string uno;
+    uno << "Milk";
+    simple_string dos;
+    dos << "Milk";
+    simple_string trs;
+    trs << "NotMilk";
     const char* qdr = "";
     const char* cnc = "";
-    const char* sis = "Filk";
-    const char* sit = "Z";
-    const char* och = "z";
-    const char* nve = "Milk ";
-
-    assert( str_cmp(uno, dos));
-    assert(!str_cmp(uno, trs));
+    simple_string sis;
+    sis << "Filk";
+    simple_string sit;
+    sit << "Z";
+    simple_string och;
+    och << "z";
+    simple_string nve;
+    nve << "Milk ";
+    assert( str_cmp(uno.string, dos.string));
+    assert(!str_cmp(uno.string, trs.string));
     assert( str_cmp(qdr, cnc));
-    assert(!str_cmp(uno, sis));
-    assert(!str_cmp(uno, qdr));
-    assert(!str_cmp(sit, och));
-    assert(!str_cmp(uno, nve));
+    assert(!str_cmp(uno.string, sis.string));
+    assert(!str_cmp(uno.string, qdr));
+    assert(!str_cmp(sit.string, och.string));
+    assert(!str_cmp(uno.string, nve.string));
     fprintf(stderr, "Test str_cmp function is OK!\n");
+    simple_string* pointers = (simple_string*) calloc(8, sizeof(simple_string));
+    pointers[0] = uno;
+    pointers[1] = dos;
+    pointers[2] = trs;
+    pointers[4] = sis;
+    pointers[5] = nve;
+    pointers[6] = sit;
+    pointers[7] = och;
+    assert(!str_cmp(pointers[0].string, "Flik"));
+    assert(str_cmp(pointers[0].string, pointers[1].string) && pointers[0].size == pointers[1].size);
+    assert(pointers[6].size == pointers[7].size);
+    free(uno.string);
+    free(dos.string);
+    free(trs.string);
+    free(sis.string);
+    free(sit.string);
+    free(och.string);
+    free(nve.string);
+    free(pointers);
+    fprintf(stderr, "Test str_cmp_4_sort function is OK!\n");
 }
 
 
 
 #endif //ONEGIN_TESTS_H
+
